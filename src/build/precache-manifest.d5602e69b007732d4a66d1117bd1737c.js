7422fe5fd50e42c0169168dc4963a332db7232af
Austen <austriel27@gmail.com>
2020-05-07 23:55:57 -0400

    First Commit

---
+++ src/build/precache-manifest.d5602e69b007732d4a66d1117bd1737c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a68591ff91a81c892080843c83c2a9df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "113e671da47be2ed9af5",
+    "url": "/static/css/main.fe6824ef.chunk.css"
+  },
+  {
+    "revision": "1b076f03553eced74c5c",
+    "url": "/static/js/2.d6e606a6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d6e606a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "113e671da47be2ed9af5",
+    "url": "/static/js/main.84b8cedc.chunk.js"
+  },
+  {
+    "revision": "053c8e34507918769709",
+    "url": "/static/js/runtime-main.aecaeffa.js"
+  }
+]);
